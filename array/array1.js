21fffe21aba09f344da28ee5b1a8fb1f1283bacc
DurbekTurgunov <durbekturgunov45@gmail.com>
2024-01-26 00:28:08 +0900

    111

---
+++ array/array1.js
@@ -0,0 +1,3 @@
+let arr = [1, 3, 5, 7, 9]
+
+console.log(arr);
---
+++ array/array2.js
@@ -0,0 +1,11 @@
+let n = 10;
+
+for (let i = 1; i < n; i++ ) {
+    if (i % n == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    console.log(i);
+    
+}
